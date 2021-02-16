5728149fd6d137c7e62e947a75cc4b7648c8fda1
Elves Sousa <elves.sousa@gmail.com>
2021-02-16 19:08:55 -0300

    Alacritty config and gracefull exit

--- configure.sh
+++ configure.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-# Paths
+# NVIM
 nvim_folder="$(pwd)/nvim"
 nvim_path="$HOME/.config/nvim"
+
+# CoC
+coc_json="$(pwd)/nvim/coc-settings.json"
+coc_path="$nvim_path/coc-settings.json"
+
+# Tmux
 tmux_file="$(pwd)/tmux/.tmux.conf"
+tmux_path="$HOME/.tmux.conf"
+
+# Git
 git_file="$(pwd)/git/.gitconfig"
+git_path="$HOME/.gitconfig"
+
+# Fish
 fish_file="$(pwd)/fish/config.fish"
 fish_path="$HOME/.config/fish/config.fish"
+
+# Terminal
 term_info="$(pwd)/terminal/xterm-256color-italic.terminfo"
-alacritty_conf="$(pwd)/alacritty/alacritty.yml"
+alacritty_file="$(pwd)/alacritty/alacritty.yml"
+alacritty_path="$HOME/.config/alacritty/alacritty.yml"
 
 # Colors and formatting
 b="$(tput bold)"
@@ -16,12 +31,26 @@ d='\033[2m'
 y='\033[33;33m'
 n='\033[0m'
 
+# Detect CTRL-C
+function exitGracefully() {
+  printf "\n Good luck."
+  exit 2
+}
+
+trap exitGracefully 2
+
 # Create symbolic links 
 function createLink() {
-  clear;
-  echo "Creating symlinks to $3 configuration file(s):"
-  echo -e " - ${d} $2 ${y}->${b} $1 ${n}"
+  if [ "$4" != "show" ]; then
+    clear;
+    echo "Creating symlinks to $3 configuration file(s):"
+  fi
+
   if type -p "$3" > /dev/null; then
+    echo -e " - ${d} $2 ${y}->${b} $1 ${n}"
+    ln -sf $1 $2
+  elif [ "$3" == 'alacritty' ]; then 
+    echo -e " - ${d} $2 ${y}->${b} $1 ${n}"
     ln -sf $1 $2
   else
     echo "Ooops: $3 not found."
@@ -51,7 +80,7 @@ function configureItalics() {
 # Instructions on screen
 clear
 echo -e "$d-------------------------------------------------------$n"
-echo -e "$b Config files install script $n"
+echo -e "$b Configuration files setup script $n"
 echo -e "$d-------------------------------------------------------$n"
 echo ' Select an action below to start:'
 echo -e " ${y}1)${n} Configure ${b}NVim${n}"
@@ -59,6 +88,7 @@ echo -e " ${y}2)${n} Configure ${b}Tmux${n}"
 echo -e " ${y}3)${n} Configure ${b}Git${n}"
 echo -e " ${y}4)${n} Configure ${b}Fish Shell${n}"
 echo -e " ${y}5)${n} Configure ${b}italics${n} in terminal"
+echo -e " ${y}6)${n} Configure ${b}Alacritty${n}"
 echo -e "$d-------------------------------------------------------$n"
 
 # Input option
@@ -69,14 +99,15 @@ while true
 do
   case $answer in
     '1')
-      createLink $nvim_folder/init.vim "$nvim_path/init.vim" 'nvim'
-      createLink $nvim_folder/colors/ $nvim_path/colors/ 'nvim'
+      createLink $nvim_folder/init.vim "$nvim_path/init.vim" 'nvim' 
+      createLink $nvim_folder/colors/ $nvim_path/colors/ 'nvim' 'show'
+      createLink $coc_json $coc_path 'nvim' 'show'
       break;;
     '2') 
-      createLink $tmux_file "$HOME/.tmux.conf" 'tmux'
+      createLink $tmux_file $tmux_path 'tmux'
       break;;
     '3') 
-      createLink $git_file "$HOME/.gitconfig" 'git'
+      createLink $git_file $git_path 'git'
       break;;
     '4') 
       createLink $fish_file $fish_path 'fish'
@@ -84,8 +115,11 @@ do
     '5') 
       configureItalics
       break;;
+    '6') 
+      createLink $alacritty_file $alacritty_path 'alacritty'
+      break;;
     *)
-      echo "Good luck."
+      echo " Good luck."
       break;
   esac
 done
