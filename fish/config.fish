c223143c2d402355538dbf47e2144368cbc633f6
Elves Sousa <elves.sousa@gmail.com>
2021-05-13 19:51:38 -0300

    RBEnv paths

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,6 @@ set PATH $PATH $HOME/.cargo/bin
 set PATH $PATH /usr/local/mysql/bin
 set PATH $PATH $HOME/Applications/bin
 set PATH $PATH $HOME/.local/bin
+set PATH $PATH $HOME/.rbenv/bin
+set PATH $PATH $HOME/.rbenv/versions/3.0.1/bin
 export PATH
