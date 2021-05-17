485a8797f0555594d810f1ff65fd85a58ac09a75
Elves Sousa <elves.sousa@gmail.com>
2021-05-17 18:55:19 -0300

    Path changes

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,5 @@ set PATH $PATH $HOME/.cargo/bin
 set PATH $PATH /usr/local/mysql/bin
 set PATH $PATH $HOME/Applications/bin
 set PATH $PATH $HOME/.local/bin
-set PATH $PATH $HOME/.rbenv/bin
-set PATH $PATH $HOME/.rbenv/versions/3.0.1/bin
+set PATH $PATH $HOME/.local/share/gem/ruby/3.0.0/bin
 export PATH
