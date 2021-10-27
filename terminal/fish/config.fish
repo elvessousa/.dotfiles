ff9d60fa20036eb920319aebaf80580488318944
Elves Sousa <elves.sousa@gmail.com>
2021-10-27 20:31:09 -0300

    Aliases for exa and bat

--- terminal/fish/config.fish
+++ terminal/fish/config.fish
@@ -1,6 +1,15 @@
+# Environment variables
 set PATH $PATH $HOME/.cargo/bin
 set PATH $PATH /usr/local/mysql/bin
 set PATH $PATH $HOME/Applications/bin
 set PATH $PATH $HOME/.local/bin
 set PATH $PATH $HOME/.local/share/gem/ruby/3.0.0/bin
 export PATH
+
+# Aliases
+alias cat='bat'
+alias l='exa'
+alias ll='exa -lah'
+alias lt='exa --tree --level=2'
+alias n='nvim .'
+alias v='vim .'
