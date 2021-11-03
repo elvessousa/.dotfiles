41a4a58ae34292b75083c042a4dfd71a22b6c3e2
Elves Sousa <elves.sousa@gmail.com>
2021-11-03 11:58:34 -0300

    Escaping spaces on file name

--- wms/sway/sway/wallpaper.sh
+++ wms/sway/sway/wallpaper.sh
@@ -3,7 +3,8 @@
 # Gets GNOME's desktop background
 picture_uri="$(dconf read /org/gnome/desktop/background/picture-uri)"
 url_to_path=${picture_uri//file:\/\//}
-wallpaper=${url_to_path//\'/}
+wallpaper=${url_to_path//%20/\\ }
 
+# echo $wallpaper
 # Changes Sway desktop background
 swaymsg output $1 bg $wallpaper fill
