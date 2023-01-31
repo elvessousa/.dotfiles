6db43a293e453ac234ede86f870c3ca9de475624
Elves Sousa <elves.sousa@gmail.com>
2023-01-31 19:23:19 -0300

    style: Fira Code

--- terminal/wezterm/wezterm-linux.lua
+++ terminal/wezterm/wezterm-linux.lua
@@ -1,8 +1,35 @@
 local wezterm = require("wezterm")
 
 return {
-	--font = wezterm.font("JetBrains Mono"),
-	font_size = 10.5,
+	--font = wezterm.font("VictorMono Nerd Font", { weight = "Medium" }),
+	font = wezterm.font("Fira Code Retina"),
+	font_size = 11.2,
+	font_rules = {
+		{
+			intensity = "Normal",
+			italic = true,
+			font = wezterm.font({
+				family = "Fira Code Retina",
+				italic = false,
+			}),
+		},
+		{
+			intensity = "Bold",
+			italic = false,
+			font = wezterm.font({
+				family = "Fira Code",
+				weight = "Bold",
+			}),
+		},
+		{
+			intensity = "Bold",
+			italic = true,
+			font = wezterm.font({
+				family = "Fira Code",
+				weight = "Bold",
+			}),
+		},
+	},
 	use_fancy_tab_bar = true,
 	hide_tab_bar_if_only_one_tab = true,
 	tab_bar_at_bottom = true,
