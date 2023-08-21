a8134a5d5a9e80ea5dcd039c247f7a1627a7fe77
Elves Sousa <elves.sousa@gmail.com>
2023-08-21 19:20:09 -0300

    script: Node packages for web development

---
+++ others/node/global-packages.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Installing Node libraries for development";
+npm install -g @astrojs/language-server emmet-ls intelephense pnpm prettier typescript typescript-language-server vscode-langservers-extracted
