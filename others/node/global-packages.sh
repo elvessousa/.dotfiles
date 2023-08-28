baba65428c9bad4391ba6e7610dce93cac2efa29
Elves Sousa <elves.sousa@gmail.com>
2023-08-28 20:39:07 -0300

    Bash language server

--- others/node/global-packages.sh
+++ others/node/global-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Installing Node libraries for development";
-npm install -g @astrojs/language-server emmet-ls intelephense pnpm prettier typescript typescript-language-server vscode-langservers-extracted
+npm install -g @astrojs/language-server bash-language-server emmet-ls intelephense pnpm prettier typescript typescript-language-server vscode-langservers-extracted
