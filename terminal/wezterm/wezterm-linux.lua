6cccd315229af8d772f57f7479c85a0399f46688
Elves Sousa <elves.sousa@gmail.com>
2025-03-29 12:09:42 -0300

    Cursor problem on Arch

--- terminal/wezterm/wezterm-linux.lua
+++ terminal/wezterm/wezterm-linux.lua
@@ -1,6 +1,21 @@
 local wezterm = require("wezterm")
 
+local xcursor_size = nil
+local xcursor_theme = nil
+
+local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-theme"})
+if success then
+  xcursor_theme = stdout:gsub("'(.+)'\n", "%1")
+end
+
+local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-size"})
+if success then
+  xcursor_size = tonumber(stdout)
+end
+
 return {
+  xcursor_theme = xcursor_theme,
+  xcursor_size = xcursor_size,
 	front_end = "WebGpu",
 	font = wezterm.font("FiraCode Nerd Font Propo"),
 	font_size = 12,
