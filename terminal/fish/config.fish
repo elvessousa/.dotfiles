ea70e050de400e719fd27f8fbb16e871b1879b35
Elves Sousa <elves.sousa@gmail.com>
2025-03-29 12:09:24 -0300

    Better ls and alias

--- terminal/fish/config.fish
+++ terminal/fish/config.fish
@@ -3,15 +3,18 @@ set PATH $PATH $HOME/.cargo/bin
 set PATH $PATH /usr/local/mysql/bin
 set PATH $PATH $HOME/Applications/bin
 set PATH $PATH $HOME/.local/bin
+set PATH $PATH $HOME/.npm-global/bin
 set PATH $PATH $HOME/.local/share/gem/ruby/3.0.0/bin
 set PATH $PATH $HOME/Android/Sdk
 export PATH
 
 # Aliases
 alias cat='bat'
-alias l='eza'
-alias ll='eza -lah'
-alias lt='eza --tree --level=2'
+alias l='eza --icons --group-directories-first'
+alias ls='eza --icons --group-directories-first'
+alias ll='eza -lah --icons --group-directories-first'
+alias lt='eza --tree --level=2 --icons'
+alias update='sudo pacman -Syu'
 alias n='nvim .'
 alias v='vim .'
 
